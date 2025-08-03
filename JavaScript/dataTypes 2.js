f86807699917cf773dbc0552ceac5b4d50f6d343
ImrenR <imrenrahbay@gmail.com>
2025-08-03 11:53:24 +0100

    p

---
+++ JavaScript/dataTypes 2.js
@@ -0,0 +1,13 @@
+// Escape Character
+
+let text2='Ben javascript\'in karakterlerini ogreniyorum'
+let text3="Seni iki sey\nHicbir sey yok" 
+
+let text4= text2.concat(text3)
+console.log(text4)
+
+let ad= "Imren"
+let soyad= "Rahbay"
+let meslek="Developer"
+
+let text9=`Personelimiz ${ad} ${soyad}, firmamiza ${meslek} olarak katilmistir`
--- JavaScript/dataTypes.js
+++
@@ -1,7 +0,0 @@
-// Escape Character
-
-let text2='Ben javascript\'in karakterlerini ogreniyorum'
-let text3="Seni iki sey\nHicbir sey yok" 
-
-let text4= text2.concat(text3)
-console.log(text4)
