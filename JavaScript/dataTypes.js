aad3066aa3f24b17dcd891c277db766addc298cf
ImrenR <imrenrahbay@gmail.com>
2025-08-03 11:49:44 +0100

    p

--- JavaScript/dataTypes.js
+++ JavaScript/dataTypes.js
@@ -1,4 +1,7 @@
 // Escape Character
 
 let text2='Ben javascript\'in karakterlerini ogreniyorum'
-let text3="Seni iki sey\nHicbir sey yok"
+let text3="Seni iki sey\nHicbir sey yok" 
+
+let text4= text2.concat(text3)
+console.log(text4)
