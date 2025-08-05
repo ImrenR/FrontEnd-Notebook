2c40b6b200dd21bcdf9be2113fb70002a657cf9e
ImrenR <imrenrahbay@gmail.com>
2025-08-05 23:05:24 +0100

    p

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -12,10 +12,13 @@ do{
   let devam= ""
 
   do{
+    let ders=prompt('Dersin adini giriniz:')
     let vize=+prompt('Vize notu gir:')
     let final=+prompt('Final tonunu giriniz:')
-    let ortalama= vize*0.4 + final*0.6
+    let ortalama= vize*0.4 + final*0.6;
 
     ortalama>=50 ?
-  `${ders}`
-  }while ()
+  `${ders} dersinden gectiniz`  :
+   `${ders} dersinden kaldiniz`
+  
+} while (devam=== "E")
