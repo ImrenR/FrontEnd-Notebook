fede4f55926bac7c4d25fcc2801d1cf4cebacf81
ImrenR <imrenrahbay@gmail.com>
2025-08-07 16:02:24 +0100

    p

--- JavaScript/Functions/functions.js
+++ JavaScript/Functions/functions.js
@@ -1,7 +1,32 @@
-// Silinidirin hacimini bulunuz 
+// Silinidirin hacimini bulunuz
 
-let h = 6
-let r = 8
+let h = 6;
+let r = 8;
 
-const hacim=(h,r)=>Math.floor(Math.PI*r*r*h)
-console.log(hacim(h,r))
+const hacim = (h, r) => Math.floor(Math.PI * r * r * h);
+console.log(hacim(h, r));
+
+// Verilen sayiya kadar asal sayilari bulma
+
+const asal = (num) => {
+  if (num < 2) {
+    console.log("Bu sayiya kadar bir asal sayi bulunamadi");
+    return;
+  }
+
+  for (let i = 2; i < num; i++) {
+    if (i === 2) {
+      console.log(i);
+    }else {
+      let entry=true // flag kullanimi
+      for (let j=2; j<i; j++){
+        if (i%j===0){
+          entry=false;
+          break
+        }
+      }
+      entry ? console.log(i) : "";
+    }
+  }
+}
+asal(10)
