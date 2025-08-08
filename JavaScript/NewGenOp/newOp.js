8698a9c19d32a381931260690418475d0cfdcfae
ImrenR <imrenrahbay@gmail.com>
2025-08-08 20:13:38 +0100

    objects

---
+++ JavaScript/NewGenOp/newOp.js
@@ -0,0 +1,28 @@
+
+// Example : 
+
+const team = [
+  {
+    name:'Imren',
+    surname:'Rahbay',
+    job: 'developer',
+    age:30,
+  },
+  { name:'John',
+    surname:'Walnut',
+    job: 'tester',
+    age:50,
+  },
+  { 
+    name:'Joline',
+    surname:'Jon',
+    job: 'Full Stack',
+    age:20,},
+];
+
+team.forEach((person)=>{
+  console.log('Name',person.name)
+}
+
+
+)
