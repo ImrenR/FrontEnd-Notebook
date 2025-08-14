4d073e9f9ded80620688ccb9de30e6d567de09c1
ImrenR <imrenrahbay@gmail.com>
2025-08-14 15:35:18 +0100

    Events

---
+++ JavaScript/Events/events.js
@@ -0,0 +1,12 @@
+//** DOM EVENTS */
+
+//? NOTES : 
+//? Dom sayesinde JS, HTML olaylari ile etkilesim halinde olabilir. 
+//? Ornegin kullanici bir HTML elemanina tikladiginda ( bir olay meydana geldiginde) bir jS kodu calistirilabilir.
+//? HTML olaylari icin bir cok ornek verilebilir. 
+//! Kullanici fareyi tikladiginda (onclick)
+//! Bir web sayfasi yuklendiginde (onload)
+//! Fare bir elemanin uzerine geldiginde (onmouseover)
+//? Ayrildiginda (onmouseout)
+//! Bir elemanin icerigi degistiginde (onchange)
+//! bir HTML formu gonderildiginde (onsubmit)
