4ff812c424d1ecafb5d00802f57ad35cc610cc04
ImrenR <imrenrahbay@gmail.com>
2025-08-18 17:45:45 +0100

    async - await

---
+++ async1/fetch-async-await.js
@@ -0,0 +1,19 @@
+//*******************. ASYNC- AWAIT *********************** */
+
+
+
+//? Async-await ECMAScript 2017. ile JS diline eklemistir.
+//? ASlinda promise yapisinin syntax olarak basitlestirilmis halidir.
+
+//! Bu baglamda syntatic sugar benzetmesi yapilabilir.
+
+//* Bir fonksiyonu async hale getirmek icin fonksiyon keyword nun onune async keyword u eklenir.
+//* Bir async fonksiyon icerisinde await keyword u ile yapilan istegin cevabinin beklenmesi saglanir.
+//* Aslinda dizilis olarak senkron mantiga benzeyen kod yazarak Asenkroon kod yazmayi mumkun kilar.
+//* Await, promise-temelli herhangi bir fonksiyonun onune getirilerek getirildigi satirdaki kodun 
+//* durdurulmasi saglar. Yapilan istek yerine getirilip sonuc degerlerinin dondurulmesi ile kodun calismasi
+//* devam eder.
+
+//! JS try anahtar kelimesi kod blogundaki kodlari calisma zamaninda test etmek icin kullanilir
+//! JS finally anahtar kelimesi hata olusmasi veya olusmamasi durumunda (her durumda) calisacak kodlari 
+//! yazdirmak icin kullanilir.
