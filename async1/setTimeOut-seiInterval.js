d25b80f9e889b5a7f469259fc735f6aae349cc20
ImrenR <imrenrahbay@gmail.com>
2025-08-18 16:35:35 +0100

    setInterval-ClearInterval

--- async1/setTimeOut-seiInterval.js
+++ async1/setTimeOut-seiInterval.js
@@ -10,15 +10,15 @@
 
 // Asenkron (1 seferlik calisan) setTimeOut
 
-setTimeout(()=>{
-  console.log("first")
-},3000); // yazi 3 sn sonra gelir consola
+// setTimeout(()=>{
+//   console.log("first")
+// },3000); // yazi 3 sn sonra gelir consola
  
-console.log("Hello") // Once hello yu print eder ardindan 3 sn sonra first gelir
+// console.log("Hello") // Once hello yu print eder ardindan 3 sn sonra first gelir
 
-setTimeout(()=>{
-  console.log("Imren")
-},2000) // ikinci yazilacak olan burasidir 2sn oldugu icin
+// setTimeout(()=>{
+//   console.log("Imren")
+// },2000) // ikinci yazilacak olan burasidir 2sn oldugu icin
 
 
 //! Notes : 
@@ -30,3 +30,14 @@ setTimeout(()=>{
 
 //? setInterval non-blocking (kod burada takili kalmaz alttaki kodlarda calisir)
 
+let sayac= 0
+
+
+const inter= setInterval(()=>{
+
+  console.log(++sayac)
+
+  if(sayac == 7){
+    clearInterval(inter)
+  }
+},1000)
