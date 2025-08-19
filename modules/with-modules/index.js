5796bd23e73f7fe472953aaeba17899313fc7a66
ImrenR <imrenrahbay@gmail.com>
2025-08-19 22:07:36 +0100

    import section

--- modules/with-modules/index.js
+++ modules/with-modules/index.js
@@ -1,13 +1,21 @@
 console.log("Hi from index.js")
 
 //* with default export
-import sayHi, { timeOfStudy } from "./myModules"
+// import sayHi, { timeOfStudy } from "./myModules"
+//or
+import sayHi,{
+increase,
+decrease,
+sumThem as topla,
+myName,
+timeOfStudy
+} from "./myModules";
 
-//* destructiong export
-import { sumThem,increase,decrease,myName } from "./myModules"
 
-console.log(sumThem(4,9));
+
+console.log(topla(4,9));
 console.log(timeOfStudy);
 console.log(increase(5));
 console.log(decrease(2)); 
-console.log(myName)
+console.log(myName);
+console.log(sayHi)
