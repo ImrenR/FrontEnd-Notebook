3f1c1af3f9e0f9d92967ba76b1d57429ba08b240
ImrenR <imrenrahbay@gmail.com>
2025-08-19 21:44:59 +0100

    importance of return

--- modules/without-modules/calculate.js
+++ modules/without-modules/calculate.js
@@ -6,6 +6,9 @@ let myName="Ipek"
 console.log(myName)
 
 
-const calculate =()=>{
+const calculate =(arr)=>{
   // we will sum the datas of products in array
+return  arr.reduce((sum,product)=> sum+product.price,0)
+// dont forget return to add, otherwise console will appear undefined
+
 }
