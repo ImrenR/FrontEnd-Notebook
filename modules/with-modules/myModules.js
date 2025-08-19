f47df5944a1a80e85047d97b7db517e3c19f5d5c
ImrenR <imrenrahbay@gmail.com>
2025-08-19 21:55:25 +0100

    myModules.js

---
+++ modules/with-modules/myModules.js
@@ -0,0 +1,16 @@
+console.log("Hi from myModules.js");
+
+//! named export
+
+export const timeOfStudy = 40;
+export function sumThem(s1, s2) {
+  return s1 + s2;
+}
+
+let counter = 5;
+
+const increase = (x) => (counter += x);
+
+const decrease = (x) => (counter -= x);
+
+const myName = "imren";
