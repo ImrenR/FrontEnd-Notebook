b77f2525cd2278222014cf06bb80c478b416693c
ImrenR <imrenrahbay@gmail.com>
2025-08-22 19:11:33 +0100

    todolist.js

---
+++ Project3/todoList.js
@@ -0,0 +1,34 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+
+const todoList = document.querySelector(".todo-list");
+
+//Event Listeners
+todoButton.addEventListener("click", addTodo);
+
+//Functions
+const addTodo = (e) => {
+  //Prevent form from submitting
+  e.preventDefault();
+  //* TODO div
+  const todoDiv = document.querySelector(".todo");
+  todoDiv.classList.add("todo"); //* todoDiv added on appjs not on html
+  //* Create Li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "hey";
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  // check mark button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = "<i class='fas fa-checked'></i>";
+  completedButton.classList.add("completed-btn");
+  todoDiv.appendChild(completedButton)
+   // check trash button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = "<i class='fas fa-trash'></i>";
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton)
+  //* Append to list
+  todoList.appendChild(todoDiv)
+};
