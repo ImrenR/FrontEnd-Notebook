c1be2fb5c8183521c081b2cc10118c5c4b447008
ImrenR <imrenrahbay@gmail.com>
2025-08-22 19:21:27 +0100

    update

--- Project3/todoList.js
+++ Project3/todoList.js
@@ -12,19 +12,19 @@ const addTodo = (e) => {
   //Prevent form from submitting
   e.preventDefault();
   //* TODO div
-  const todoDiv = document.querySelector(".todo");
+  const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo"); //* todoDiv added on appjs not on html
   //* Create Li
   const newTodo = document.createElement("li");
   newTodo.innerText = "hey";
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
-  // check mark button
+  //* check mark button
   const completedButton = document.createElement("button");
-  completedButton.innerHTML = "<i class='fas fa-checked'></i>";
+ completedButton.innerHTML = "<i class='fas fa-checked'></i>";
   completedButton.classList.add("completed-btn");
   todoDiv.appendChild(completedButton)
-   // check trash button
+   //* check trash button
   const trashButton = document.createElement("button");
   trashButton.innerHTML = "<i class='fas fa-trash'></i>";
   trashButton.classList.add("trash-btn");
@@ -32,3 +32,5 @@ const addTodo = (e) => {
   //* Append to list
   todoList.appendChild(todoDiv)
 };
+
+//. 25dk dakika
