74b7c452d34d11771626855276d689f78560766f
ImrenR <imrenrahbay@gmail.com>
2025-08-23 15:20:21 +0100

    recap

--- Project3/todoList.js
+++ Project3/todoList.js
@@ -1,66 +1,59 @@
 // Selecetors
-
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
 //Event listeners
 todoButton.addEventListener("click", addTodo);
-todoList.addEventListener("click", deleteCheck);
 //! In JavaScript, variables declared with const or let are not hoisted like function declarations.
-
+//! therefore if you are going to declare your function with const you need to place above at the bottom of the function
 // Functions
 function addTodo(e) {
   // Prevent form from submitting
   e.preventDefault();
 
-  //* Todo Div
-  const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todo");
+//* Todo Div
+  const todoDiv = document.createElement('div')
+  todoDiv.classList.add('todo')
 
   // Create Li
-  const newTodo = document.createElement("li");
-  newTodo.innerText = todoInput.value;
-  newTodo.classList.add("todo-item");
-  todoDiv.appendChild(newTodo);
-
+  const newTodo = document.createElement("li")
+  newTodo.classList.add("todo-item")
+  newTodo.innerText = "hey";
+  todoDiv.appendChild(newTodo)
   // check mark button
-  const completedButton = document.createElement("button");
-  completedButton.innerHTML = '<i class="fas fa-check"></i>';
-  completedButton.classList.add("complete-btn");
-  todoDiv.appendChild(completedButton);
 
-  // trash button
-  const trashButton = document.createElement("button");
-  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-  trashButton.classList.add("trash-btn");
-  todoDiv.appendChild(trashButton);
+  const completeButton = document.createElement("button")
+  completeButton.innerHTML= '<i class="fas fa-check" ></i>'
+  completeButton.classList.add("complete-btn")
+  todoDiv.appendChild(completeButton)
 
-  //Append to List
-  todoList.appendChild(todoDiv);
 
-  //Clear todo input value
-  todoInput.value = "";
+// trash button
+
+  const trashButton = document.createElement("button")
+  trashButton.innerHTML= '<i class="fas fa-trash" ></i>'
+  trashButton.classList.add("trash-btn")
+  todoDiv.appendChild(trashButton)
+
+//Append to List
+todoList.appendChild(todoDiv)
+
 }
 
-function deleteCheck(a) {
-  const item = a.target;
-  // DELETE TODO
 
-  if (item.classList[0] === "trash-btn") {
-    const todo = item.parentElement; // goes one level up in the DOM
-    // Animation
-    todo.classList.add("fall");
-    todo.addEventListener('transitionend', function(){
-      todo.remove();
-    })
-  }
 
-  //Check mark
-   if (item.classList[0] === "complete-btn") {
-    const todo = item.parentElement; // goes one level up in the DOM
-    todo.classList.toggle('completed'); // If the element does NOT have the completed class → add it.
-  }
-}
+
+
+
+
+//Clear todo input value
+
+// DELETE TODO
+
+// goes one level up in the DOM
+// Animation
+
+//Check mark
 
 //48:20
