8a58f6938a038dac4c58f456c9fcc636c7267446
ImrenR <imrenrahbay@gmail.com>
2025-09-03 20:24:33 +0100

    folder fixes with vite

--- events-note/src/App.jsx
+++ events-note/src/App.jsx
@@ -11,8 +11,8 @@ function App() {
       {/* <h1>React Events</h1> */}
       {/* <Events/> */}
       {/* <UseStateHook/> */}
-      <DataStorage/> 
-      
+       <DataStorage/> 
+     
     </div>
   );
 }
