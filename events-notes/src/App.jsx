12e10a59dc260596d3a18e52e6dce8dad1cb49c2
ImrenR <imrenrahbay@gmail.com>
2025-09-03 19:34:08 +0100

    add

---
+++ events-notes/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Events from './components/events/Events';
+import DataStorage from './components/useStateHook/DataStorage';
+import UseStateHook from './components/useStateHook/UseStateHook';
+
+function App() {
+
+  return (
+    <div >
+     {/* <h1>EVENTS NOTES</h1> */} 
+
+     {/*<Events/>*/} 
+     {/*<UseStateHook/>
+     */}
+     <DataStorage/>
+    </div>
+  );
+}
+
+export default App;
