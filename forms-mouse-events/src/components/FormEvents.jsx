e33cc517993fd16f8ee4fd7689dd06d2693b3dd0
ImrenR <imrenrahbay@gmail.com>
2025-09-03 22:23:31 +0100

    bootstrap dowlanded

---
+++ forms-mouse-events/src/components/FormEvents.jsx
@@ -0,0 +1,13 @@
+
+
+const FormEvents = () => {
+  return (
+    <div className="mt-4 p-3">
+      <div className="text-center">
+        <h2>FORM EVENTS</h2>
+      </div>
+    </div>
+  );
+};
+
+export default FormEvents;
