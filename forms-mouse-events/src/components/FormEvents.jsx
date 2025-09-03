dd9e8ec810d63d3f06192650265dce8e7f5ff37b
ImrenR <imrenrahbay@gmail.com>
2025-09-03 22:30:20 +0100

    bootstrap form

--- forms-mouse-events/src/components/FormEvents.jsx
+++ forms-mouse-events/src/components/FormEvents.jsx
@@ -1,11 +1,33 @@
-
-
 const FormEvents = () => {
   return (
     <div className="mt-4 p-3">
       <div className="text-center">
         <h2>FORM EVENTS</h2>
       </div>
+      <form action="">
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@example.com"
+          />
+        </div>
+      </form>
+      <form action="">
+        <label for="inputPassword5" class="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          id="inputPassword5"
+          class="form-control"
+          aria-describedby="passwordHelpBlock"
+        />
+      </form>
     </div>
   );
 };
