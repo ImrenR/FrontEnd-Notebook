411f8faecbc967a139ea2b5d31bec66a59bf8560
ImrenR <imrenrahbay@gmail.com>
2025-09-05 20:05:11 +0100

    update

--- useEffectHook/src/App.jsx
+++ useEffectHook/src/App.jsx
@@ -1,13 +1,20 @@
 
 import './App.css'
 import UseEffectHooks from './components/UseEffectHooks'
+import { useState } from 'react';
+import User from './components/User'
 
 function App() {
  
-
+const [visible, setVisible] = useState(true)
   return (
-    <div>
-      <UseEffectHooks/>
+    <div className="container text-center mt-4">
+      <button className="btn btn-danger"
+      onClick={()=>setVisible(!visible)}> 
+        {visible ? "Gizle" : "Goster"}
+      </button>
+        {/* <UseEffectHooks/> */}
+     { visible &&  <User/>}
     </div>
   )
 }
---
+++ useEffectHook/src/components/User.jsx
@@ -0,0 +1,16 @@
+const User = () => {
+  return (
+  <div>
+    <img src="" alt="" />
+    <p>Hi My name is</p>
+    <h2>User name</h2>
+    <h4>email</h4>
+    <h5>d.tarihi</h5>
+    <h5>sehir</h5>
+    <h5>tel</h5>
+    <button className="btn btn-warning">New User</button>
+  </div>
+)
+}
+
+export default User
