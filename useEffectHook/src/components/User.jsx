fb7f26f452c49d2cd71b1bab31f7f8cf054a07ce
ImrenR <imrenrahbay@gmail.com>
2025-09-05 20:38:19 +0100

    mini project final- User.jsx

--- useEffectHook/src/components/User.jsx
+++ useEffectHook/src/components/User.jsx
@@ -1,47 +1,51 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-
 const User = () => {
- const [userData, setUserData] = useState()
+  const [userData, setUserData] = useState(null); {/*ilk renderde state degeri initial  
+    deger olacagi icin hata almamak icin ya state e initial deger verilmeli (null)
+     ya da optional chaning ?  kontrolu yapmaliyiz asagida yaptigim gibi*/}
+ // const getUser = ()=> {
+  //  axios
+  //  .get("https://randomuser.me/api/")
+  //  .then((result)=>console.log(result.data.results[0]))
+  //  .catch((error)=> console.log(error))
+  //  }
+  // }
+  const getUser = async () => {
+    try {
+      const result = await axios.get("https://randomuser.me/api/");
+      setUserData(result.data.results[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
- const getUser = async()=> {
- try{ 
-  const result = await axios.get("https://randomuser.me/api/");
- setUserData(result.data.results[0])}
- catch (error){
-  console.log(error)
- }
-};
-
-useEffect(() => {
-  getUser();
-}, []) // api ya istek atma ve sonsuz donuden cikma
-
-// const getUser = ()=> {
-//  axios
-//  .get("https://randomuser.me/api/")
-//  .then((result)=>console.log(result.data.results[0]))
-//  .catch((error)=> console.log(error))
-//  }
-// }
-
-
-//! fetch ile axios benzer
-//! async-await ile then-catch benzer
+  useEffect(() => {
+    getUser();
+  }, []); // api ya istek atma ve run once on mount //! fetch ile axios benzer //! async-await ile then-catch benzer
 
   return (
-  <div>
-    <img src="" alt="" />
-    <p>Hi My name is</p>
-    <h2>User name</h2>
-    <h4>email</h4>
-    <h5>d.tarihi</h5>
-    <h5>sehir</h5>
-    <h5>tel</h5>
-    <button className="btn btn-warning">New User</button>
-  </div>
-)
-}
+    <div className="text-center">
+      
+      {userData ? (
+       
+       <>
+          <img src={userData.picture.large}  alt="" />
+          <p>Hi My name is {userData.name.first}</p>
+          <h2>Username: {userData.login.username}</h2>
+          <h4>Email :{userData.email}</h4>
+          <h5>Birthdate: {new Date(userData.dob.date).toLocaleDateString()}</h5>
+          <h5>City: {userData.location.city}</h5>
+          <h5>Phone: {userData.phone}</h5>
+          <button className="btn btn-warning"
+          onClick={getUser}>New User</button>
+        </>
+      ) : (
+        <p>Loading user...</p>
+      )}
+    </div>
+  );
+};
 
-export default User
+export default User;
