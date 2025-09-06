b1dd025a17baa806e5b6e785fac9dbdff5fdfdaa
ImrenR <imrenrahbay@gmail.com>
2025-09-06 17:15:11 +0100

    html+css done done(w bootstrap-icons)

--- useEffect-Axios-Example/src/components/TutorialList.jsx
+++ useEffect-Axios-Example/src/components/TutorialList.jsx
@@ -1,12 +1,15 @@
+import { CiEdit } from "react-icons/ci";
+import { MdOutlineDeleteForever } from "react-icons/md";
+
 const TutorialList = () => {
 
 
 
-  
+
   return (
 
 
-  <div>
+  <div className="container mt-4">
  <table class="table">
         <thead>
           <tr>
@@ -21,7 +24,11 @@ const TutorialList = () => {
             <th scope="row">1</th>
             <td>Mark</td>
             <td>Otto</td>
-            <td>@mdo</td>
+            <td className="">
+              <CiEdit type="button" size={20} className="text-warning"/>
+              <MdOutlineDeleteForever type="button" size={20} className="text-danger"/>
+
+              </td>
           </tr>
         </tbody>
       </table>
