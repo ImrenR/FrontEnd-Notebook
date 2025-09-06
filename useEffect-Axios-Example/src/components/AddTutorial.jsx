eb9cfeb4336bf7e17d4b3128af4b45dd046ff3bc
ImrenR <imrenrahbay@gmail.com>
2025-09-06 17:09:18 +0100

    react-icons uploaded

--- useEffect-Axios-Example/src/components/AddTutorial.jsx
+++ useEffect-Axios-Example/src/components/AddTutorial.jsx
@@ -1,16 +1,37 @@
-
-
-
 const AddTutorial = () => {
   return (
-  <div>
-   <div>
-    <p>Add Your Tutorial</p>
-    
-   </div>
+    <div>
+      <h1 className="container text-danger">Add Your Tutorial</h1>
+      <form>
+        <div class="mb-3">
+          <label for="title" class="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="title"
+            placeholder="Enter your title"
+          />
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">
+            Description
+          </label>
+          <textarea
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            rows="2"
+            placeholder="Enter your description"
+          ></textarea>
+        </div>
+        <button type="button" class="btn btn-primary">
+          Submit
+        </button>
+      </form>
+     
+    </div>
+  );
+};
 
-  </div>
-  )
-}
-
-export default AddTutorial
+export default AddTutorial;
--- useEffect-Axios-Example/src/components/TutorialList.jsx
+++ useEffect-Axios-Example/src/components/TutorialList.jsx
@@ -1,5 +1,31 @@
 const TutorialList = () => {
-  return <div>TutorialList</div>
+
+
+
+  
+  return (
+
+
+  <div>
+ <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col" className="text-center">Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </table>
+  </div>)
 }
 
 export default TutorialList
