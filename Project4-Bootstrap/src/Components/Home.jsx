e4bf1a1ea3b06677e661a5e069f17e14c75c1745
ImrenR <imrenrahbay@gmail.com>
2025-09-09 13:46:35 +0100

    update intro bootstrap -vitejs

--- Project4-Bootstrap/src/Components/Home.jsx
+++ Project4-Bootstrap/src/Components/Home.jsx
@@ -1,8 +1,30 @@
 const Home = () => {
   return (
-  <div>
-<h1 className="text-center bg-dark text-warning p-3 ">Bootstrap Vite Js</h1>
-  </div>
-)}
+    <div>
+      <h1 className="text-center bg-dark text-warning p-3 ">
+        Bootstrap Vite Js
+      </h1>
+      <p className="h1">Lorem ipsum dolor sit amet</p>
+      <p className="h2">Lorem ipsum dolor sit amet</p>
+      <p className="h3">Lorem ipsum dolor sit amet</p>
+      <p className="h4">Lorem ipsum dolor sit amet</p>
+      <p className="h5">Lorem ipsum dolor sit amet</p>
+      <p className="h6">Lorem ipsum dolor sit amet</p>
 
-export default Home
+      <p className="display-1">Lorem ipsum dolor sit amet.</p>
+      <p className="display-2">Lorem ipsum dolor sit amet.</p>
+      <p className="display-3">Lorem ipsum dolor sit amet.</p>
+      <p className="display-4">Lorem ipsum dolor sit amet.</p>
+      <p className="display-5">Lorem ipsum dolor sit amet.</p>
+      <p className="display-6">Lorem ipsum dolor sit amet.</p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate et
+        atque nesciunt eveniet <span className="fs-1">odio placeat sit in a delectus</span>, adipisci beatae
+        ex molestiae ipsa, harum molestias expedita eos at esse.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
