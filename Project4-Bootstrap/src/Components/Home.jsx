f0a9fe0351bb77b353aef1877f6175cd46a1585f
ImrenR <imrenrahbay@gmail.com>
2025-09-09 13:37:36 +0100

    bootstrap intro

--- Project4-Bootstrap/src/Components/Home.jsx
+++ Project4-Bootstrap/src/Components/Home.jsx
@@ -1,5 +1,8 @@
 const Home = () => {
-  return <div>Home</div>
-}
+  return (
+  <div>
+<h1 className="text-center bg-dark text-warning p-3 ">Bootstrap Vite Js</h1>
+  </div>
+)}
 
 export default Home
